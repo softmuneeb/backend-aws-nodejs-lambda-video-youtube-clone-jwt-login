a22408948ed0629ae7dadfaa9c70aba4b70ca57a
Muneeb <softmuneeb@gmail.com>
2023-08-08 15:57:58 +0500

    generalize port for railway.app

--- index.js
+++ index.js
@@ -105,7 +105,7 @@ app.get('/', (req, res) => {
 });
 
 function startServer() {
-  const PORT = 3000;
+  const PORT = process.env.PORT || 4000;
   app.listen(PORT, () => {
     console.log(`Server started on http://localhost:${PORT}`);
   });
